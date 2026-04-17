a9e08c47918fd944347a3c4334de26f51fe624e5
Sophie <109428682+phizzii@users.noreply.github.com>
2026-04-17 17:44:10 +0100

    add all route files, modify index js file to ensure database paths are not ambiguous

--- server/db/db.js
+++ server/db/db.js
@@ -5,11 +5,22 @@ const fs = require('fs');
 
 console.log("2: modules loaded");
 
+const path = require('path');
+
+const schemaPath = path.join(__dirname, 'schema.sql');
+const dbPath = path.join(__dirname, 'database.db')
+
 const schema = fs.readFileSync('./db/schema.sql', 'utf8');
 
 console.log("3: schema loaded");
 
-const db = new sqlite3.Database('./database.db');
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.log('connected to sqlite database');
+    // foreign keys must be enabled per connection
+    db.run('PRAGMA foreign_keys = ON')
+  }
+});
 
 db.exec(schema, (err) => {
   if (err) {
